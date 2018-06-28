20d01111f4d91ee44efa73535cb6e304a8e116b6
Apirut Vanchaam <apirut@live.com>
2018-06-28 23:13:17 +0700

    host

--- test_post.py
+++ test_post.py
@@ -1,33 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+import requests
 
-app = Flask(__name__)
+remote_url = "http://192.168.1.182:5000/first_call?id=12345
+post_data = {'name','Art'}
 
-@app.route('/first_call', methods=['POST'])
-def first_callback():
-    if request.method == 'POST':
-        result = request.data
-        id = request.args.get("id")
-        name = request.form.get('name')
-        client_ip = request.remote_addr
-        print('client IP:',client_ip)
-        print('id:', id)
-        print('name:', name)
-        print('client data:',request.json)
-
-    return "OK"
-
-
-@app.route('/second_call', methods=['GET'])
-def second_callback():
-    if request.method == 'GET':
-         #....
-         pass
-
-    return "OK"
-
-if __name__ == '__main__':
-    app.debug = True
-    app.run("127.0.0.1")
-
+r = requests.post(remote_url,post_data)
+print(r.status, r.reason)
+print(r.text)
