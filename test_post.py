579809a9e12eaea6061f4089783b29d8075cd8c5
Apirut Vanchaam <apirut@live.com>
2018-06-28 23:24:15 +0700

    fdjksfj

--- test_post.py
+++ test_post.py
@@ -2,9 +2,9 @@
 
 import requests
 
-remote_url = "http://192.168.1.182:5000/first_call?id=12345
-post_data = {'name','Art'}
+remote_url = "http://192.168.1.182:5000/first_call?id=12345"
+post_data = {'name':'Art'}
 
 r = requests.post(remote_url,post_data)
-print(r.status, r.reason)
+print(r.status_code, r.reason)
 print(r.text)
